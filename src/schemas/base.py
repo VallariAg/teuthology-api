3b4339ef8ab393554125f82b678a58cc865ec1a1
Vallari Agrawal <val.agl002@gmail.com>
2022-08-19 19:00:45 +0530

    Add schemas for base args, suite cmd args, and schedule args
    TODO: schedule args might need to be changed. Placeholder args for now.
    
    Signed-off-by: Vallari Agrawal <val.agl002@gmail.com>

---
+++ src/schemas/base.py
@@ -0,0 +1,9 @@
+from typing import Union, Optional
+from pydantic import BaseModel, Field
+
+
+class BaseArgs(BaseModel):
+    dry_run: Union[bool, None] = Field(default=False, alias="--dry-run")
+    non_interactive: Union[bool, None] = Field(default=False, alias="--non-interactive")
+    verbose: Union[int, None] = Field(default=1, alias="--verbose")
+    help: Union[bool, None] = Field(default=False, alias="--help")
