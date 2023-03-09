fe3795539d6dd8dec83cdbae3c26afd8539c1c77
Med16-11 <singhmedhavi923@gmail.com>
2023-03-09 20:27:15 +0530

    Added unit test  for error handling.

---
+++ src/test_error_handling.py
@@ -0,0 +1,23 @@
+from fastapi.testclient import TestClient
+import pytest
+from app import app
+
+client = TestClient(app)
+
+def test_invalid_job_id():
+    response = client.get("/jobs/1234")
+    assert response.status_code == 404
+    assert response.json() == {"detail": "Job not found"}
+
+def test_missing_parameter():
+    response = client.post("/jobs/run", json={})
+    assert response.status_code == 422
+    assert response.json() == {
+        "detail": [
+            {
+                "loc": ["body", "config"],
+                "msg": "field required",
+                "type": "value_error.missing"
+            }
+        ]
+    }
