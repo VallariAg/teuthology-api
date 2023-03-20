e7aa1208e6d215f5a81f844b9f275539773e2ba2
Devansh3712 <devanshamity@gmail.com>
2023-03-21 00:04:00 +0530

    Update tests
    Signed-off-by: Devansh3712 <devanshamity@gmail.com>

--- tests/test_main.py
+++ tests/test_main.py
@@ -12,7 +12,9 @@ def test_root():
 def test_kill_no_auth():
     response = client.post("/kill")
     assert response.status_code == 401
+    assert response.json() == {"detail": "You need to be logged in"}
 
 def test_suite_no_auth():
     response = client.post("/suite")
     assert response.status_code == 401
+    assert response.json() == {"detail": "You need to be logged in"}
