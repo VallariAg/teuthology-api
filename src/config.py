c3023c1cf64813663142dd289177daa934ffe2e2
Devansh3712 <devanshamity@gmail.com>
2023-07-14 22:01:12 +0530

    src/config.py: Update to pydantic_settings for BaseSettings class

--- src/config.py
+++ src/config.py
@@ -1,32 +1,21 @@
 from functools import lru_cache
-from pydantic import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class APISettings(BaseSettings):
-    """
-    Class for API settings.
-    """
+    """Class for API settings."""
+
+    model_config = SettingsConfigDict(env_file='.env', env_file_encoding='utf-8')
 
     PADDLES_URL: str = "http://paddles:8080"
     # TODO: team names need to be changed below when created
     admin_team: str = "ceph"  # ceph's github team with *sudo* access to sepia
     teuth_team: str = "teuth"  # ceph's github team with access to sepia
 
-    class Config:
-        """
-        Class for Config.
-        """
-
-        # pylint: disable=too-few-public-methods
-        env_file = ".env"
-        env_file_encoding = "utf-8"
-
 
 @lru_cache()
 def get_api_settings() -> APISettings:
-    """
-    Returns the API settings.
-    """
+    """Returns the API settings."""
     return APISettings()  # reads variables from environment
 
 
