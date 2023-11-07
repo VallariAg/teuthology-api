d8d43463f5c274d05c08591e04ec414bd18ae758
Vallari Agrawal <val.agl002@gmail.com>
2023-11-07 20:18:53 +0530

    start_container.sh: fix 'Could not import module "main"' for uvicorn server
    fixes: https://github.com/ceph/teuthology-api/issues/35
    
    Signed-off-by: Vallari Agrawal <val.agl002@gmail.com>

--- start_container.sh
+++ start_container.sh
@@ -9,7 +9,7 @@ PORT=${TEUTHOLOGY_API_SERVER_PORT:-"8080"}
 cd /teuthology_api/src/
 
 if [ "$DEPLOYMENT" = "development" ]; then
-  uvicorn main:app --reload --port $PORT --host $HOST
+  uvicorn teuthology_api.main:app --reload --port $PORT --host $HOST
 else
   gunicorn -c /teuthology_api/gunicorn_config.py teuthology_api.main:app
 fi
